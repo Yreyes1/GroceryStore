68f036132e6fe6f123e64de37585a5d016024659
yreyes1 <yreyes1@example.com>
2018-02-27 11:28:03 -0800

    Display items

--- main.cpp
+++ main.cpp
@@ -36,6 +36,12 @@ int main()
   }
   }while (input != 'q' && input != 'Q');
 
+  cout<<"==ITEMS TO BUY==\n";
+  for(int index = 0; index < 5; index ++)
+  {
+    cout<<index+1<<" " <<list[index]<<endl;
+  }  
+
   
   
   return 0;
