fdefaded79b1bb88a6fa863a8c8e1de56173d053
yreyes1 <yreyes1@example.com>
2018-03-13 11:50:30 -0700

    Updated grocery store

--- main.cpp
+++ main.cpp
@@ -1,48 +1,56 @@
 // Author:Yaneth Reyes Hinojosa
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
-  string item;
+        vector <string> groceryList; // vector of strings
+        int numItems = 0;
+        char input;
+        string item;
 
-  do 
-  {
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>> input;
+        do 
+        {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>> input;
 
-  if(input == 'a'|| input == 'A')
-  {
-    cout<< "What is the item?\n";
-    cin >> item;
-  if(numItems < 5)
-  {
-    list[numItems]=item;
-    numItems++;
-  }  
-  else 
-  {
-     cout<<"You'll need a bigger list!\n";
-  
-  }
-  }
-  }while (input != 'q' && input != 'Q');
+                if(input == 'a'|| input == 'A')
+                {
+                        cout<< "What is the item?\n";
+                        cin >> item;
+                        groceryList.push_back(item);
+                }
 
-  cout<<"==ITEMS TO BUY==\n";
-  for(int index = 0; index < 5; index ++)
-  {
-    cout<<index+1<<" " <<list[index]<<endl;
-  }  
 
-  
-  
-  return 0;
+                if(input != 'a' && input != 'A' && input != 'q' && input != 'Q')
+                {
+                        cout<<"That is not a valid input";
+                }        
+
+        }while (input != 'q' && input != 'Q');
+
+        if(groceryList.empty() == false)
+        {
+             cout<<"==ITEMS TO BUY==\n";
+        }
+
+        if(groceryList.empty() == true)
+        {
+            cout<<"No items to buy!\n";
+        }
+        for(int index = 0; index < groceryList.size(); index ++)
+        {
+              cout<< index + 1;
+              cout<<" " <<groceryList[index]<<endl;
+        }  
+
+
+
+        return 0;
 }
